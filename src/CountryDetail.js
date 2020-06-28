60a41a49bd198c9bc5067bf7ec19a62b816701cc
Tim Beattie <trbeattie@outlook.com>
2020-06-28 16:22:21 -0400

    Obtain country from worldCountries dataset based on URL parameter

--- src/CountryDetail.js
+++ src/CountryDetail.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import worldCountries from 'world-countries';
+
+function findCountry(countries, cca3) {
+    /* 
+     * Search a list of countries conforming to the world-countries dataset
+     * (https://github.com/mledoze/countries) and return the country with a
+     * matching CCA3 ID.
+     * */
+
+    for (let country of countries) {
+        if (country.cca3 === cca3.toUpperCase()) {
+            // Country found in dataset
+            return country;
+        }
+    }
+
+    // Country not found in dataset
+    throw new Error(
+        `Country with CCA3 code ${cca3} not found in provided dataset.`
+    );
+}
 
 function CountryDetail() {
 
     // CountryId passed in as URL parameter: /countries/:countryId
     const { countryId } = useParams();
+
+    let country = {};
+    
+    try {
+        country = findCountry(worldCountries, countryId);
+    }  catch(e) {
+        return <p>Could not find country {countryId}!</p>
+    }
   
     return(
-    <p>You requested: {countryId}</p>
+    <p>You requested: {country.name.common}</p>
     );
   }
 
