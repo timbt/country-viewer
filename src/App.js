efd3e1bb0a141fe1008cd7e63f113d6ea86d0c81
Tim Beattie <trbeattie@outlook.com>
2020-06-27 21:23:06 -0400

    Implement url path parsing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useRouteMatch,
+  useParams
+} from 'react-router-dom';
+
+function CountryDetail() {
+
+  const { countryId } = useParams();
+
+  return(
+  <p>You requested: {countryId}</p>
+  );
+}
+
+function Country() {
+
+  const match = useRouteMatch();
+
+  return (
+    <Switch>
+      <Route path={`${match.path}/:countryId`}>
+        <CountryDetail /> 
+      </Route>
+    </Switch>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello world!</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route path ='/countries'>
+          <Country />
+        </Route>
+        <Route path='/'>
+          <p>Hello world!</p>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
