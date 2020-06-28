d1bc240fbebe23ecc14007589eb40955d9e6c5a1
Tim Beattie <trbeattie@outlook.com>
2020-06-28 14:23:16 -0400

    Updates

---
+++ precache-manifest.ff48db18facf1df5a1b37b3dc7a53a77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd25989cffba4945370e731487c23d0",
+    "url": "/country-viewer/index.html"
+  },
+  {
+    "revision": "947da9b49060283cc19b",
+    "url": "/country-viewer/static/js/2.d72f2b10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/country-viewer/static/js/2.d72f2b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0afb2a97d9d74f73155f",
+    "url": "/country-viewer/static/js/main.f6df3b36.chunk.js"
+  },
+  {
+    "revision": "c2b80db5645059b8362a",
+    "url": "/country-viewer/static/js/runtime-main.96f4220e.js"
+  }
+]);
