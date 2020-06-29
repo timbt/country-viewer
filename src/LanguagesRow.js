c5518c3503eaff19cb9b38256d5573841849dd32
Tim Beattie <trbeattie@outlook.com>
2020-06-29 16:35:03 -0400

    Add conditional rendering to languages row

--- src/LanguagesRow.js
+++ src/LanguagesRow.js
@@ -5,10 +5,21 @@ function LanguagesRow({ languages }) {
     // Extract names of languages from provided languages object
     let languageList = Object.values(languages);
 
+    // Do not render the row if there is no data to display
+    if (languageList.length === 0) {
+        return null;
+    }
+
+    // Set the row's name as singular or plural based on the number of
+    // languages provided.
+    const rowName = languageList.length === 1 ? 
+        'Official Language' : 
+        'Official Languages'
+
     // Return a table row with a comma seperated list of languages
     return (
         <tr>
-            <td>Official Languages</td>
+            <td>{rowName}</td>
             <td>{languageList.join(', ')}</td>
         </tr>
     );
